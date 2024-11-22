c13a5f5c4514de30cde7de9d23dd7b7c05f1c6c6
furuta <git@furuta.me>
2024-11-22 10:10:40 +0900

    first commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by furuta on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
